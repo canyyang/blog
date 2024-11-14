efdd8002c9237ebb3eb0f837e6a16687752eb4aa
canyyang <canyangchen@126.com>
2024-11-14 10:08:25 +0800

    UPDATE card background

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,14 +20,11 @@ export default function Home() {
                                     </Button>))}
         </Flex>
       </Flex>
-      <Flex vertical={true} align='center' className={style.fixbox} style={{bottom: '15px'}}>
-        <a href="http://beian.miit.gov.cn/" className={style.record}>粤ICP备2024334937号</a>
-      </Flex>
       <div className={style.leftbox}></div>
       <Flex className={style.rightbox} vertical={true} align='center'>
         <Flex justify='space-between' style={{width: '850px'}}>
           <Card className={`${style.card} ${style['left-card']}`} style={{width: '400px', height: '200px'}}>
-            <Flex align='center' className={`${style['intro-title']}`}><img src='imgs/blacks.png' style={{width: '25px', marginRight: '5px'}} /> 阿阳@canyyang</Flex>
+            <Flex align='center' className={`${style['intro-title']}`}><img src='imgs/yang.png' style={{width: '25px', marginRight: '5px'}} /> 阿阳@canyyang</Flex>
             <div className={`${style['intro-context']}`}>前端开发者/在读研究生/代码魔法家</div>
             <Flex justify='flex-end'><Button type='text' className={`${style['left-button']}`}>了解更多</Button></Flex>
           </Card>
@@ -64,6 +61,9 @@ export default function Home() {
           <Flex className={`${style.card}  ${style['contact']}`} style={{width: '400px', marginTop: '23px'}}><div className={`${style['contact-img']}`} style={{backgroundPosition: '0 0'}}></div><span> Email: canyangchen@126.com</span></Flex>
           <Flex className={`${style.card}  ${style['contact']}`} style={{width: '400px', marginTop: '23px'}}><div className={`${style['contact-img']}`} style={{backgroundPosition: '-32px 0'}}></div><span> QQ: 2357873118</span></Flex>
         </Flex>
+      </Flex>
+      <Flex vertical={true} align='center' className={style.fixbox} style={{bottom: '15px'}}>
+        <a href="http://beian.miit.gov.cn/" className={style.record}>粤ICP备2024334937号</a>
       </Flex>
     </Flex>
   )
